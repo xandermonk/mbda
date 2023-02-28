ee93fc133efa62e274e477885697548df1015979
xpetersen <ptrsnxndr@gmail.com>
2023-02-28 11:14:54 +0100

    init

---
+++ app/src/main/java/com/example/mbda_assessment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mbda_assessment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
